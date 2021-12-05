2684fbda414ad8d2ac936a739569df5c407b3d53
Lluís Cortès <lluiscortes@uoc.edu>
2021-12-05 22:38:36 +0100

    Gallery page

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -9,6 +9,26 @@ import * as bootstrap from 'bootstrap';
 // FontAwesome
 import '@fortawesome/fontawesome-free/js/all.js';
 
+// Simple Lightbox
+import SimpleLightbox from 'simplelightbox';
+new SimpleLightbox('.gallery-item', {
+  captions: false,
+  
+});
+
+// Images Loaded
+import imagesLoaded from 'imagesloaded';
+
+// Masonry Layout
+import Masonry from 'masonry-layout';
+var gallery = document.getElementById('gallery');
+imagesLoaded(gallery,function(){
+  var msnry = new Masonry(gallery,{
+    percentPosition: true,
+  });
+});
+
+
 /**
  * Write any other JavaScript below
  */
