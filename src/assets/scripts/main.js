c81487d461d02963b79b3a71b982f49494b9284e
Lluís Cortès <lluiscortes@uoc.edu>
2021-12-06 00:06:36 +0100

    Header

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,16 +11,28 @@ import '@fortawesome/fontawesome-free/js/all.js';
 
 // Simple Lightbox
 import SimpleLightbox from 'simplelightbox';
-new SimpleLightbox('.gallery-item', {
-  captions: false,
-  
-});
 
 // Images Loaded
 import imagesLoaded from 'imagesloaded';
 
 // Masonry Layout
 import Masonry from 'masonry-layout';
+
+// AOS
+import AOS from 'aos';
+
+
+
+/**
+ * Write any other JavaScript below
+ */
+
+// Simple Lightbox
+new SimpleLightbox('.gallery-item', {
+  captions: false,
+});
+
+// Masonry
 var gallery = document.getElementById('gallery');
 imagesLoaded(gallery,function(){
   var msnry = new Masonry(gallery,{
@@ -28,7 +40,32 @@ imagesLoaded(gallery,function(){
   });
 });
 
+// AOS
+AOS.init({
+  duration: 1000,
+});
 
-/**
- * Write any other JavaScript below
- */
+
+// Header behavior
+const toggle = document.getElementById('toggle');
+const navbar = document.getElementById('navbar');
+const navLinks = document.getElementsByClassName('navbar__link');
+const openMenu = () => {
+  navbar.classList.add('navbar--show');
+  toggle.classList.add('toggle--close');
+}
+const closeMenu = () => {
+  navbar.classList.remove('navbar--show');
+  toggle.classList.remove('toggle--close');
+}
+const toggleMenu = () => {
+  if (navbar.classList.contains('navbar--show')) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+}
+toggle.addEventListener('click', toggleMenu);
+for (var i = 0, len = navLinks.length; i < len; i++) {
+  navLinks[i].addEventListener('click', closeMenu);
+}
